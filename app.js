97796adf24f68186adff0a554d4b72f5f663ae8e
zaamir88 <zaamircanas@gmail.com>
2025-02-15 22:37:08 -0600

    prueba para hacer commit

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let amigos = [];
 
+/**
+ * Agrega un nuevo amigo al array de amigos.
+ * Valida que el nombre no esté vacío antes de agregarlo y actualiza la lista visual.
+ */
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById("amigo").value;
+
+  if (nombreAmigo.trim() === "") {
+    alert("Por favor, inserte un nombre");
+  } else {
+    amigos.push(nombreAmigo);
+    document.querySelector("#amigo").value = "";
+    mostrarListaAmigo();
+  }
+}
+
+/**
+ * Actualiza la visualización de la lista de amigos en el DOM, creando elementos <li> para cada amigo.
+ */
+function mostrarListaAmigo() {
+  let listaAmigos = document.querySelector("#listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let index = 0; index < amigos.length; index++) {
+    const element = amigos[index];
+
+    let listaHTML = document.createElement("li");
+    listaHTML.textContent = element;
+    listaAmigos.appendChild(listaHTML);
+  }
+}
+
+/**
+ * Sortea y muestra un amigo de la lista de amigos de manera aleatoria.
+ * Verifica que la lista no esté vacía antes de realizar el sorteo.
+ */
+function sortearAmigo() {
+  let cantidadAmigos = amigos.length;
+  if (cantidadAmigos === 0) {
+    alert("Por favor, inserte un nombre antes de sortear");
+  } else {
+    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+    let resultadoHTML = document.querySelector("#resultado");
+    resultadoHTML.innerHTML = amigos[indiceAmigo];
+  }
+}
